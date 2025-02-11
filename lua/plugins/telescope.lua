3b271fa93ef353f546f42be6c0d8223f889c6555
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-02-11 16:19:32 +0545

    small changes

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,3 +1,5 @@
+local map = vim.keymap.set
+
 return {
   'nvim-telescope/telescope.nvim',
   event = 'VimEnter',
@@ -81,19 +83,19 @@ return {
 
     -- See `:help telescope.builtin`
     local builtin = require 'telescope.builtin'
-    vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-    vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
-    vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-    vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-    vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-    vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-    vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-    vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-    vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-    vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+    map('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    map('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+    map('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+    map('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+    map('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+    map('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+    map('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+    map('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+    map('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    map('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
     -- Slightly advanced example of overriding default behavior and theme
-    vim.keymap.set('n', '<leader>/', function()
+    map('n', '<leader>/', function()
       -- You can pass additional configuration to Telescope to change the theme, layout, etc.
       builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
         winblend = 10,
@@ -103,7 +105,7 @@ return {
 
     -- It's also possible to pass additional configuration options.
     --  See `:help telescope.builtin.live_grep()` for information about particular keys
-    vim.keymap.set('n', '<leader>s/', function()
+    map('n', '<leader>s/', function()
       builtin.live_grep {
         grep_open_files = true,
         prompt_title = 'Live Grep in Open Files',
