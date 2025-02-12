32e96ab01bf3ac2e6dc75478db3a1ee91b4ee7a4
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-02-12 20:04:45 +0545

    background transparency can be toggled

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,8 +1,13 @@
 return {
    'Exafunction/codeium.vim',
+	dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
   event = 'BufEnter',
   config = function ()
     vim.keymap.set('n' , '<leader>cc', '<cmd>Codeium Toggle<CR>', { noremap = true, silent = true } )
 
   end
 }
+
--- lua/plugins/colortheme.lua
+++ lua/plugins/colortheme.lua
@@ -1,8 +1,28 @@
+local bg_transparent = false 
+
 return {
   'olimorris/onedarkpro.nvim',
   lazy = false,
   priority = 1000,
   config = function()
+    require("onedarkpro").setup({
+      options = {
+        transparency = bg_transparent,
+      }
+    })
     vim.cmd("colorscheme onedark_dark")
+
+    local toggle_transparency = function()
+      bg_transparent = not bg_transparent
+      require("onedarkpro").setup({
+        options = {
+          transparency = bg_transparent,
+        }
+      })
+      vim.cmd("colorscheme onedark_dark")
+    end 
+
+    vim.keymap.set('n', '<leader>bg', toggle_transparency, { noremap = true, silent = true, desc = "Toggle background transparency" })
   end,
 }
+
