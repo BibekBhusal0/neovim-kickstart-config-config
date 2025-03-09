4142e54444ed10c4dc34f2d12133152e1341f8c4
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-03-09 13:08:23 +0545

    ✨ Feat: Added leetcode keybindings

--- lua/plugins/leetcode.lua
+++ lua/plugins/leetcode.lua
@@ -1,4 +1,5 @@
 local leet_arg = 'leetcode.nvim'
+local map = require 'utils.map'
 
 return {
   'kawre/leetcode.nvim',
@@ -7,5 +8,20 @@ return {
     { '<leader>lC', ':Leet<cr>', desc = 'Leetcode dashboard' },
   },
   lazy = leet_arg ~= vim.fn.argv(0, -1),
-  opts = { arg = leet_arg },
+  config = function()
+    require('leetcode').setup { arg = leet_arg }
+    map('<leader>lcc', ':Leet console<cr>', 'Leetcoce console')
+    map('<leader>lcd', ':Leet daily<cr>', 'Leetcode proplem of the day')
+    map('<leader>lci', ':Leet info<cr>', 'Leetcode problem info')
+    map('<leader>lcl', ':Leet lang<cr>', 'Leetcode change language')
+    map('<leader>lcL', ':Leet last_submit<cr>', 'Leetcode last submit')
+    map('<leader>lco', ':Leet open<cr>', 'Leetcode in browser')
+    map('<leader>lcp', ':Leet list<cr>', 'Leetcode problem')
+    map('<leader>lcP', ':Leet tabs<cr>', 'Leetcode opened problems')
+    map('<leader>lcr', ':Leet random<cr>', 'Leetcode random problem')
+    map('<leader>lcR', ':Leet reset<cr>', 'Leetcode reset')
+    map('<leader>lcs', ':Leet submit<cr>', 'Leetcode submit')
+    map('<leader>lct', ':Leet test<cr>', 'Leetcode test')
+    map('<leader>lcy', ':Leat yank<cr>', 'Leetcode yank soultion')
+  end,
 }
