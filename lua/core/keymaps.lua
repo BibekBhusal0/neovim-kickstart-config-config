61dae41af0f019c726fc813403411f7eef1d4300
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-03-15 10:30:20 +0545

    smart delete added

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -68,3 +68,19 @@ map('<leader>rn', ':set rnu!<CR>', 'Toggle Relative Line Number')
 
 map('zo', 'za', 'Toggle fold')
 map('<Esc>', '<Cmd>noh<CR>', 'Clear Highlight')
+
+-- https://www.reddit.com/r/neovim/comments/1janrmf/smart_delete/
+local function smart_delete(key)
+  local l = vim.api.nvim_win_get_cursor(0)[1]
+  local line = vim.api.nvim_buf_get_lines(0, l - 1, l, true)[1]
+  return (line:match '^%s*$' and '"_' or '') .. key
+end
+
+local keys = { 'd', 'dd', 'x', 'c', 's', 'C', 'S', 'X' }
+
+-- Set keymaps for both normal and visual modes
+for _, key in pairs(keys) do
+  vim.keymap.set({ 'n', 'v' }, key, function()
+    return smart_delete(key)
+  end, { noremap = true, expr = true, desc = 'Smart delete' })
+end
