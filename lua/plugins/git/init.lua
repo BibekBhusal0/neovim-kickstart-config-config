9f0c22047733a51aff36f1ead22d888ed5c1f149
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-23 13:41:19 +0545

    git pipeline added

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -2,6 +2,7 @@ return {
   require "plugins.git.diffview",
   require "plugins.git.git",
   require "plugins.git.gitsigns",
+  require "plugins.git.pipeline",
   -- require "plugins.git.lazygit",
   -- require "plugins.git.tardis",
   -- require "plugins.git.worktree",
---
+++ lua/plugins/git/pipeline.lua
@@ -0,0 +1,11 @@
+local wrap_keys = require "utils.wrap_keys"
+
+return {
+  {
+    'topaxi/pipeline.nvim',
+    keys = wrap_keys {
+      { '<leader>ci', '<cmd>Pipeline<cr>', desc = 'Open pipeline.nvim' },
+    },
+    opts = {},
+  },
+}
