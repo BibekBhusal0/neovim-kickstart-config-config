232bc32fdfb43ada723cab68d82c28a35ba86569
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-05-23 13:44:14 +0545

    renamed git to fugitive

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -1,6 +1,6 @@
 return {
   require "plugins.git.diffview",
-  require "plugins.git.git",
+  require "plugins.git.fugitive",
   require "plugins.git.gitsigns",
   require "plugins.git.pipeline",
   -- require "plugins.git.lazygit",
