7e61cd19f1880b971ba6f9d7a89b612b15f54113
BibekBhusal0 <bibekbhusal04@gmail.com>
2025-08-24 22:40:53 +0545

    update: telescope icon picker shortcut

--- lua/plugins/telescope/icon-picker.lua
+++ lua/plugins/telescope/icon-picker.lua
@@ -18,8 +18,7 @@ return {
   keys = wrap_keys {
     { "<leader>fe", ":PickEmoji<CR>", desc = "Icon Picker Emoji" },
     { "<leader>fE", ":PickEmojiYank emoji<CR>", desc = "Icon Picker Emoji Yank" },
-    { "<leader>fi", ":PickIcons<CR>", desc = "Icon Picker" },
-    { "<leader>fI", ":PickIconsYank<CR>", desc = "Icon Picker Yank" },
+    { "<leader>fI", ":PickIcons<CR>", desc = "Icon Picker" },
     { "<leader>fS", ":PickSymbols<CR>", desc = "Icon Picker Unicode Symbols" },
   },
   dependencies = { "nvim-telescope/telescope-ui-select.nvim" },
