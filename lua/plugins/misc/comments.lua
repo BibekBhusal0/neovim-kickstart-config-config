4f69edd3167a1ac2bb7b1126334abaff3b6a3329
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-08 17:02:57 +0545

    feat: keymap to remove all comments

--- lua/plugins/misc/comments.lua
+++ lua/plugins/misc/comments.lua
@@ -1,6 +1,48 @@
 local map = require "utils.map"
 local wrap_keys = require "utils.wrap_keys"
 
+function RemoveAllComments()
+  local bufnr = 0
+  local parser = vim.treesitter.get_parser(bufnr)
+  if (not parser) then return end
+  local tree = parser:parse()[1]
+  local root = tree:root()
+
+  local query = vim.treesitter.query.parse(
+    parser:lang(),
+    [[
+    (comment) @comment
+  ]]
+  )
+
+  local comments = {}
+  for _, node in query:iter_captures(root, bufnr) do
+    local start_row, start_col, end_row, end_col = node:range()
+    table.insert(comments, { start_row, start_col, end_row, end_col })
+  end
+
+  table.sort(comments, function(a, b)
+    if a[1] == b[1] then
+      return a[2] > b[2]
+    end
+    return a[1] > b[1]
+  end)
+
+  for _, range in ipairs(comments) do
+    local start_row, start_col, end_row, end_col = range[1], range[2], range[3], range[4]
+
+    if start_row == end_row then
+      local line = vim.api.nvim_buf_get_lines(bufnr, start_row, start_row + 1, false)[1]
+      local new_line = line:sub(1, start_col) .. line:sub(end_col + 1)
+      vim.api.nvim_buf_set_lines(bufnr, start_row, start_row + 1, false, { new_line })
+    else
+      vim.api.nvim_buf_set_text(bufnr, start_row, start_col, end_row, end_col, {})
+    end
+  end
+end
+
+map("<leader>rc", RemoveAllComments, "Remove all comments")
+
 return {
   {
     {
