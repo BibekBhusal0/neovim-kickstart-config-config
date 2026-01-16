cd959a729c65926c6ef544f3aca8c0afbf465bf9
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-17 00:07:40 +0545

    refactor: ♻️ move commands from here to there

--- lua/plugins/git/fugitive.lua
+++ lua/plugins/git/fugitive.lua
@@ -1,54 +1,10 @@
 local wrap_keys = require "utils.wrap_keys"
 local map = require "utils.map"
 
-local function parse(text)
-  local handle = io.popen("devmoji --text " .. vim.fn.shellescape(text))
-  if handle then
-    local emojified_text = handle:read "*a"
-    handle:close()
-    emojified_text = emojified_text:match "^%s*(.-)%s*$"
-    return emojified_text
-  else
-    return text
-  end
-end
-
-local function commit_with_message()
-  require "utils.commit_input"(" Commit Changes ", function(text)
-    vim.system({ "git", "commit", "-m", parse(text) }):wait()
-  end)
-end
-
-local function commit_all_with_message()
-  require "utils.commit_input"(" Add and Commit ", function(text)
-    vim.system({ "git", "add", "." }):wait()
-    vim.system({ "git", "commit", "-m", parse(text) }):wait()
-  end)
-end
-
-local function change_last_commit_message()
-  local handle = io.popen "git log -1 --pretty=%B"
-  if not handle then
-    return
-  end
-  local message = handle:read "*a"
-  handle:close()
-  local m = message:match "^%s*(.-)%s*$"
-  if not m then
-    return
-  end
-  require "utils.commit_input"(" Change Commit Message ", function(text)
-    vim.system({ "git", "commit", "--amend", "-m", parse(text) }):wait()
-  end, m)
-end
-
 map("<leader>g/", ":Git stash<CR>", "Git stash")
 map("<leader>g[", ":Git push --force<CR>", "Git push Force")
 map("<leader>gA", ":Git add %<CR>", "Git add current file")
 map("<leader>ga", ":Git add .<CR>", "Git add all files")
-map("<leader>gc", commit_all_with_message, "Git commit all")
-map("<leader>gC", commit_with_message, "Git commit")
-map("<leader>ge", change_last_commit_message, "Git change last commit message")
 map("<leader>gi", ":Git init<CR>", "Git Init")
 map("<leader>gJ", ":Git commit --amend --no-edit<CR>", "Git commit to last commit")
 map("<leader>gj", ":Git commit -a --amend --no-edit<CR>", "Git add and commit to last commit")
--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -1,4 +1,46 @@
 local map = require "utils.map"
+
+local function parse(text)
+  local handle = io.popen("devmoji --text " .. vim.fn.shellescape(text))
+  if handle then
+    local emojified_text = handle:read "*a"
+    handle:close()
+    emojified_text = emojified_text:match "^%s*(.-)%s*$"
+    return emojified_text
+  else
+    return text
+  end
+end
+
+local function commit_with_message()
+  require "utils.commit_input"(" Commit Changes ", function(text)
+    vim.system({ "git", "commit", "-m", parse(text) }):wait()
+  end)
+end
+
+local function commit_all_with_message()
+  require "utils.commit_input"(" Add and Commit ", function(text)
+    vim.system({ "git", "add", "." }):wait()
+    vim.system({ "git", "commit", "-m", parse(text) }):wait()
+  end)
+end
+
+local function change_last_commit_message()
+  local handle = io.popen "git log -1 --pretty=%B"
+  if not handle then
+    return
+  end
+  local message = handle:read "*a"
+  handle:close()
+  local m = message:match "^%s*(.-)%s*$"
+  if not m then
+    return
+  end
+  require "utils.commit_input"(" Change Commit Message ", function(text)
+    vim.system({ "git", "commit", "--amend", "-m", parse(text) }):wait()
+  end, m)
+end
+
 local function open_changed_files_in_buffers()
   -- Get the list of changed and untracked files from git
   local handle = io.popen "git ls-files --modified --others --exclude-standard"
@@ -27,6 +69,9 @@ local function open_changed_files_in_buffers()
 end
 
 vim.api.nvim_create_user_command("GitChanges", open_changed_files_in_buffers, {})
+map("<leader>gc", commit_all_with_message, "Git commit all")
+map("<leader>gC", commit_with_message, "Git commit")
+map("<leader>ge", change_last_commit_message, "Git change last commit message")
 map("<leader>gg", ":GitChanges<CR>", "Git open changes")
 
 return {
