c6f5586eaed2a7a8689d90a9ab6774c56754d385
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-18 15:12:35 +0545

    feat: ✨ use my plugin to repleace long funcitons

---
+++ lua/plugins/git/utils.lua
@@ -0,0 +1,27 @@
+local wrap_keys = require "utils.wrap_keys"
+
+return {
+  "bibekbhusal0/nvim-git-utils",
+  dir = "~/Code/nvim-git-utils",
+  opts = { commit_input = { hints = false } },
+  cmd = {
+    "GitAddCommit",
+    "GitCommit",
+    "GitChangeLastCommit",
+    "GitChanges",
+    "DiffviewCompareBranchesTelescope",
+    "DiffviewFileHistoryTelescope",
+  },
+  keys = wrap_keys {
+    { "<leader>gc", ":GitAddCommit<CR>", desc = "Git add and commit" },
+    { "<leader>gC", ":GitCommit<CR>", desc = "Git commit" },
+    { "<leader>ge", ":GitChangeLastCommit<CR>", desc = "Git Change last commit message" },
+    { "<leader>gg", ":GitChanges<CR>", desc = "Git open changed files" },
+    { "<leader>gdb", ":DiffviewCompareBranchesTelescope<CR>", desc = "Diffview Compare Branches" },
+    {
+      "<leader>gdF",
+      ":DiffviewFileHistoryTelescope<CR>",
+      desc = "Diffview File history Telescope",
+    },
+  },
+}
