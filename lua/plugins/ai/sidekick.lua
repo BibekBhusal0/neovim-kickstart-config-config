fc61639e9ca128ca6cfe5a9a5efc8a75213de3b2
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-22 14:57:52 +0545

    refactor: ♻️ remove sidekick nes

--- lua/plugins/ai/sidekick.lua
+++ lua/plugins/ai/sidekick.lua
@@ -6,17 +6,6 @@ return {
   opts = {},
   keys = wrap_keys {
     {
-      "<leader><tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require("sidekick").nes_jump_or_apply() then
-          return "<Tab>" -- fallback to normal tab
-        end
-      end,
-      expr = true,
-      desc = " Sidekick Goto/Apply Next Edit Suggestion",
-    },
-    {
       "<c-.>",
       function()
         require("sidekick.cli").toggle()
