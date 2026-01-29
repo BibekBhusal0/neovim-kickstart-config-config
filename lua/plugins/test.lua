5a46eb7de4331378fb01b813017ee1ab5fa88721
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-30 01:46:13 +0545

    simple improvements in test.

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,5 +1,4 @@
 local wrap_keys = require "utils.wrap_keys"
--- TODO: Needs Test are not tested needs testings
 
 return {
   "nvim-neotest/neotest",
@@ -22,18 +21,22 @@ return {
     { "<leader>Tr", ':lua require("neotest").run.run()<CR>', desc = "Test Run" },
     {
       "<leader>TR",
-      ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>',
+      ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>:lua require("neotest").summary.open()<CR>',
       desc = "Test Run File",
     },
     { "<leader>TS", ':lua require("neotest").run.stop()<CR>', desc = "Test Stop" },
     { "<leader>To", ':lua require("neotest").output.open()<CR>', desc = "Test Ouput" },
     { "<leader>Tp", ':lua require("neotest").output_panel.toggle()<CR>', desc = "Test Panel" },
     { "<leader>Ts", ':lua require("neotest").summary.toggle()<CR>', desc = "Test Summary" },
+    { "<leader>Tw", ':lua require("neotest").watch.toggle()<CR>', desc = "Test Watch Toggle" },
   },
 
   config = function()
     require("neotest").setup {
       adapters = { require "neotest-bun" },
+      floating = { border = "rounded" },
+      output = { open_on_run = false },
+      summary = { open = "botright vsplit | vertical resize 30" },
     }
   end,
 }
