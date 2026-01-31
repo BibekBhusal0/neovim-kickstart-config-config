733d2638ce378e8e02ff086fcf9bd4de1b5c6b1f
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-01-31 21:27:52 +0545

    test key maps change.
    - `<leader>Tr` to run current file.
    - `<leader>TR` to run under cursor.
    - `<leader>Ta` to run all.
    - `<laeder>TW` to toggle watch all.
    
    Also open summary after those commands.

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,5 +1,6 @@
 local wrap_keys = require "utils.wrap_keys"
 
+local summary = ':lua require("neotest").summary.open()<CR>'
 return {
   "nvim-neotest/neotest",
   dependencies = {
@@ -12,23 +13,40 @@ return {
   },
 
   keys = wrap_keys {
-    { "<leader>Ta", ':lua require("neotest").run.attach()<CR>', desc = "Test Attach" },
+    {
+      "<leader>Ta",
+      ':lua require("neotest").run.run(vim.fn.getcwd()) <CR>' .. summary,
+      desc = "Test Run All",
+    },
     {
       "<leader>Td",
       ':lua require("neotest").run.run({strategy = "dap"})<CR>',
       desc = "Test Debug",
     },
-    { "<leader>Tr", ':lua require("neotest").run.run()<CR>', desc = "Test Run" },
     {
       "<leader>TR",
-      ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>:lua require("neotest").summary.open()<CR>',
+      ':lua require("neotest").run.run()<CR>' .. summary,
+      desc = "Test Run",
+    },
+    {
+      "<leader>Tr",
+      ':lua require("neotest").run.run(vim.fn.expand("%"))<CR>' .. summary,
       desc = "Test Run File",
     },
     { "<leader>TS", ':lua require("neotest").run.stop()<CR>', desc = "Test Stop" },
     { "<leader>To", ':lua require("neotest").output.open()<CR>', desc = "Test Ouput" },
     { "<leader>Tp", ':lua require("neotest").output_panel.toggle()<CR>', desc = "Test Panel" },
     { "<leader>Ts", ':lua require("neotest").summary.toggle()<CR>', desc = "Test Summary" },
-    { "<leader>Tw", ':lua require("neotest").watch.toggle()<CR>', desc = "Test Watch Toggle" },
+    {
+      "<leader>Tw",
+      ':lua require("neotest").watch.toggle()<CR>' .. summary,
+      desc = "Test Watch Toggle",
+    },
+    {
+      "<leader>TW",
+      ':lua require("neotest").watch.toggle(vim.fn.getcwd())<CR>' .. summary,
+      desc = "Test Watch Toggle All",
+    },
   },
 
   config = function()
