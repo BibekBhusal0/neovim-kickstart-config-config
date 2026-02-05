9c54cef5cd89396bd7142bd98cac8cd6830353a9
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-05 20:40:40 +0545

    feat: ✨ theme for zoxide finder telescope

--- lua/plugins/telescope/zoxide.lua
+++ lua/plugins/telescope/zoxide.lua
@@ -1,15 +1,16 @@
 local wrap_keys = require "utils.wrap_keys"
 
+vim.api.nvim_create_user_command("Zoxide", function()
+  require("telescope").extensions.zoxide.list { theme = "dropdown", previewer = false }
+end, {})
+
 return {
   "jvgrootveld/telescope-zoxide",
   keys = wrap_keys {
-    {
-      "<leader>fz",
-      ":lua require('telescope').extensions.zoxide.list()<CR>",
-      desc = "Find zoxide folders",
-    },
+    { "<leader>fz", ":Zoxide<CR>", desc = "Find zoxide directories" },
+    { "<leader>cd", ":Zoxide<CR>", desc = "Change Directory" },
   },
   config = function()
-    require("telescope").load_extension "zoxide"
+    require("telescope").setup { extensions = { zoxide = { prompt_title = "Directories" } } }
   end,
 }
