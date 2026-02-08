9f5b874649e35ad38246adf73890caf3db82e366
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-08 21:41:45 +0545

    using Navigator.nvim to tmux navigation (works for terminal mode as well)

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -1,5 +1,6 @@
 local wrap_keys = require "utils.wrap_keys"
 
+local modes = { "v", "n", "t" }
 return {
   {
     enabled = os.getenv "TMUX" ~= nil,
@@ -13,21 +14,24 @@ return {
   }, -- Merge Status line of neovim and tmux
 
   {
-    "christoomey/vim-tmux-navigator",
+    "numToStr/Navigator.nvim",
     cmd = {
-      "TmuxNavigateLeft",
-      "TmuxNavigateDown",
-      "TmuxNavigateUp",
-      "TmuxNavigateRight",
-      "TmuxNavigatePrevious",
-      "TmuxNavigatorProcessList",
+      "NavigatorLeft",
+      "NavigatorDown",
+      "NavigatorUp",
+      "NavigatorRight",
+      "NavigatorPrevious",
+      "NavigatorProcessList",
     },
     keys = wrap_keys {
-      { "<C-h>", ":TmuxNavigateLeft<CR>", desc = "Window Left" },
-      { "<C-j>", ":TmuxNavigateDown<CR>", desc = "Window Down" },
-      { "<C-k>", ":TmuxNavigateUp<CR>", desc = "Window Up" },
-      { "<C-l>", ":TmuxNavigateRight<CR>", desc = "Window Right" },
-      { "<C-\\>", ":TmuxNavigatePrevious<CR>", desc = "Window Previous" },
+      { "<C-h>", "<C-\\><C-N><CMD>NavigatorLeft<CR>", desc = "Window Left", mode = modes },
+      { "<C-j>", "<C-\\><C-N><CMD>NavigatorDown<CR>", desc = "Window Down", mode = modes },
+      { "<C-k>", "<C-\\><C-N><CMD>NavigatorUp<CR>", desc = "Window Up", mode = modes },
+      { "<C-l>", "<C-\\><C-N><CMD>NavigatorRight<CR>", desc = "Window Right", mode = modes },
+      { "<C-\\>", "<C-\\><C-N><CMD>NavigatorPrevious<CR>", desc = "Window Previous", mode = modes },
     },
+    config = function()
+      require("Navigator").setup()
+    end,
   },
 }
