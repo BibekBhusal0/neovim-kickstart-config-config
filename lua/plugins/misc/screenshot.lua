70318ebe337062bd00c4fbca201f077fd9d3e349
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-09 17:50:59 +0545

    feat: ✨ using snap for screenshot instead of silicon

--- lua/plugins/misc/screenshot.lua
+++ lua/plugins/misc/screenshot.lua
@@ -1,73 +1,43 @@
 local wrap_keys = require "utils.wrap_keys"
-local get_file_name = function()
-  return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()), ":t")
+local screenshotFolder = "~/Code/Screenshots/"
+
+---Take Screenshot after setting up snap's config
+---@param config SnapUserConfig to overwrite
+local function snap(config)
+  require("snap.config").set(config)
+  local mode = vim.api.nvim_get_mode().mode
+  if mode:match "^v" or mode:match "^V" then
+    vim.cmd "'<,'>Snap"
+    return
+  end
+  vim.cmd "Snap"
 end
 
-local get_time_stamp = function()
-  return os.date "!%Y-%m-%d_%H-%M-%S"
+local function snap_to_desktop()
+  snap {
+    save_to_disk = { image = true },
+    copy_to_clipboard = { image = false },
+  }
 end
 
-local get_file_name_and_timestamp = function()
-  return get_file_name() .. "_" .. get_time_stamp()
-end
-
-local function take(opts)
-  local sil = require "nvim-silicon"
-  local options = vim.tbl_deep_extend("force", sil.options, opts)
-  sil.shoot(options)
-end
-
-local save_to_desktop = function()
-  local home = vim.fn.expand "~"
-  take {
-    output = home .. "/Code/Screenshots/" .. get_file_name_and_timestamp() .. ".png",
-    to_clipboard = false,
+local function snap_to_clipboard()
+  snap {
+    save_to_disk = { image = false },
+    copy_to_clipboard = { image = true },
   }
 end
 
 return {
   {
-    "michaelrommel/nvim-silicon",
-    cmd = "Silicon",
-    main = "nvim-silicon",
-
+    "mistweaverco/snap.nvim",
+    cmd = "Snap",
     keys = wrap_keys {
-      { "<leader>sc", ":Silicon<CR>", desc = "Screenshot to clipboard", mode = { "n", "v" } },
-      { "<leader>sC", save_to_desktop, desc = "Screenshot to clipboard", mode = { "n", "v" } },
+      { "<leader>sc", snap_to_clipboard, desc = "Screenshot to clipboard", mode = { "n", "v" } },
+      { "<leader>sC", snap_to_desktop, desc = "Screenshot to desktop", mode = { "n", "v" } },
     },
-
     opts = {
-      line_offset = function(args)
-        if args.range == 0 or args.range == nil then
-          return 1
-        end
-        return args.line1
-      end,
-
-      font = "JetBrainsMono Nerd Font",
-      theme = "DarkNeon",
-      -- background = '#f76161',
-      shadow_blur_radius = 14,
-      to_clipboard = true,
-      shadow_offset_x = 20,
-      shadow_offset_y = 20,
-      shadow_color = "#27272a",
-      background_image = vim.fn.stdpath "config" .. "/gradient.png",
-      num_separator = "\u{258f}",
-      output = nil,
-
-      window_title = function()
-        local repo_url = vim.fn.system "git config --get remote.origin.url"
-        if repo_url and repo_url ~= "" then
-          repo_url = vim.fn.trim(repo_url)
-          local username, repo_name = repo_url:match "https://github.com/([^/]+)/([^/]+)"
-          if username and repo_name then
-            repo_name = repo_name:gsub("%.git$", "")
-            return ("  " .. username .. "/" .. repo_name)
-          end
-        end
-        return "   " .. get_file_name()
-      end,
+      output_dir = screenshotFolder,
+      filename_pattern = "%file_name.%file_extension-%t",
     },
   },
 }
