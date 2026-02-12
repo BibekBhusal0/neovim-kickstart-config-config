5946350564accf1ef939fc79dfba6eac9ad4b188
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-12 11:30:58 +0545

    fix: 🐛 tmux lualine

--- lua/plugins/misc/tmux.lua
+++ lua/plugins/misc/tmux.lua
@@ -1,17 +1,23 @@
 local wrap_keys = require "utils.wrap_keys"
 
 local modes = { "v", "n", "t" }
+local function hide_lualine()
+  vim.o.laststatus = 0
+end
+
 return {
   {
     enabled = os.getenv "TMUX" ~= nil,
     "vimpostor/vim-tpipeline",
     config = function()
-      vim.o.laststatus = 0
+      hide_lualine()
+      vim.opt.fillchars:append { stl = "━", stlnc = "━" }
       vim.g.tpipeline_autoembed = 1
       vim.g.tpipeline_restore = 0
+      vim.g.tpipeline_focuslost = 1
       vim.g.tpipeline_clearstl = 1
-      vim.g.tpipeline_focuslost = 0
-      vim.opt.fillchars:append { stl = "━", stlnc = "━" }
+
+      vim.api.nvim_create_autocmd({ "BufEnter", "FileType", "WinEnter" }, { callback = hide_lualine })
     end,
   }, -- Merge Status line of neovim and tmux
 
