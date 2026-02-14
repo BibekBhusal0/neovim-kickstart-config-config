5dcf1b1be61eb7e3e566b6a0f4200fd67621d404
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-14 15:55:36 +0545

    feat: ✨ `<leader>Sc` to open screenshot dir

--- lua/plugins/misc/screenshot.lua
+++ lua/plugins/misc/screenshot.lua
@@ -1,4 +1,7 @@
+local map = require "utils.map"
 local utils = require "utils.screenshot"
+local screenshot_dir = "~/Code/Screenshots/"
+map("<leader>Sc", ":silent !nautilus " .. screenshot_dir .. "&<CR>", "Open Screenshot directory")
 
 return {
   {
@@ -7,7 +10,7 @@ return {
     opts = {
       templateFilepath = vim.fn.stdpath "config" .. "/snap/template.html",
       save_to_disk = { image = true },
-      output_dir = "~/Code/Screenshots/",
+      output_dir = screenshot_dir,
       filename_pattern = "%file_name.%file_extension-%t",
       additional_template_data = utils.defaultTemplate,
     },
