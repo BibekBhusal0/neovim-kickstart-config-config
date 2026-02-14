0d49ade54e57c8901dc24ba4872b570c6eb5c481
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-02-15 00:00:45 +0545

    fix: 🐛 get correct repo for screenshot

--- lua/utils/screenshot.lua
+++ lua/utils/screenshot.lua
@@ -24,7 +24,31 @@ end
 ---Get current github repo
 ---@return string
 function M.get_repo()
-  local repo_url = vim.fn.system "git config --get remote.origin.url"
+  local file = vim.api.nvim_buf_get_name(0)
+  local repo_root = ""
+  if file and file ~= "" then
+    repo_root = vim.fn.systemlist({
+      "git",
+      "-C",
+      vim.fn.fnamemodify(file, ":p:h"),
+      "rev-parse",
+      "--show-toplevel",
+    })[1] or ""
+  else
+    repo_root = vim.fn.systemlist({ "git", "rev-parse", "--show-toplevel" })[1] or ""
+  end
+
+  local repo_url
+  if repo_root and repo_root ~= "" then
+    repo_url = vim.fn.system {
+      "git",
+      "-C",
+      repo_root,
+      "config",
+      "--get",
+      "remote.origin.url",
+    }
+  end
   if repo_url and repo_url ~= "" then
     repo_url = vim.fn.trim(repo_url)
     local username, repo_name = repo_url:match "https://github.com/([^/]+)/([^/]+)"
