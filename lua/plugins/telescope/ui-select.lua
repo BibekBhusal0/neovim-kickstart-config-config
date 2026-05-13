3e88d739d47dc31ae2e8807d934d36f1540e2d4d
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-05-13 14:17:04 +0545

    fix: 🐛 ui select lazy loading

--- lua/plugins/telescope/ui-select.lua
+++ lua/plugins/telescope/ui-select.lua
@@ -1,10 +1,19 @@
+local original_select = vim.ui.select
+vim.ui.select = function(...)
+  local ok, telescope = pcall(require, "telescope")
+  if ok then
+    telescope.setup {
+      extensions = { ["ui-select"] = require("telescope.themes").get_dropdown() },
+    }
+    pcall(telescope.load_extension, "ui-select")
+    if vim.ui.select ~= original_select then
+      return vim.ui.select(...)
+    end
+  end
+  return original_select(...)
+end
+
 return {
   "nvim-telescope/telescope-ui-select.nvim",
   lazy = true,
-  config = function()
-    require("telescope").setup {
-      extensions = { ["ui-select"] = require("telescope.themes").get_dropdown() },
-    }
-    require("telescope").load_extension "ui-select"
-  end,
 }
