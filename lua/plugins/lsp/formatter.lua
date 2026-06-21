5b45b373c4a40e23d817a5415e945bdb7ed543a5
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-21 17:33:23 +0545

    feat: ✨ range wise formatting

--- lua/plugins/lsp/formatter.lua
+++ lua/plugins/lsp/formatter.lua
@@ -1,19 +1,26 @@
 local wrap_keys = require "utils.wrap_keys"
 
-local function format()
-  require("conform").format()
-end
+vim.api.nvim_create_user_command("Format", function(args)
+  local range = nil
+  if args.count ~= -1 then
+    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+    range = { start = { args.line1, 0 }, ["end"] = { args.line2, end_line:len() } }
+  end
+  require("conform").format { async = true, lsp_format = "fallback", range = range }
+end, { range = true })
 
 return {
   "stevearc/conform.nvim",
   dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
   event = "LspAttach",
-  keys = wrap_keys { { "<leader>F", format, desc = "Format Using LSP" } },
+
+  keys = wrap_keys {
+    { "<leader>F", ":Format<Cr>", desc = "Format Using LSP", mode = { "n", "v" } },
+  },
   config = function()
     require("mason-tool-installer").setup { "stylua", "prettier" }
 
-    local conform = require "conform"
-    conform.setup {
+    require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettier" },
