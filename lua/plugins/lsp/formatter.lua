03379764d5e897901c3125a862a8b8d05a496be2
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-06-21 18:07:54 +0545

    fix(formatter): 🐛 prettier format for all types

--- lua/plugins/lsp/formatter.lua
+++ lua/plugins/lsp/formatter.lua
@@ -13,18 +13,30 @@ return {
   "stevearc/conform.nvim",
   dependencies = { "WhoIsSethDaniel/mason-tool-installer.nvim" },
   event = "LspAttach",
-
   keys = wrap_keys {
     { "<leader>F", ":Format<Cr>", desc = "Format Using LSP", mode = { "n", "v" } },
   },
-  config = function()
-    require("mason-tool-installer").setup { "stylua", "prettier" }
 
+  config = function()
+    require("mason-tool-installer").setup { ensure_installed = { "stylua", "prettier" } }
     require("conform").setup {
       formatters_by_ft = {
         lua = { "stylua" },
         javascript = { "prettier" },
-        md = { "prettier" },
+        markdown = { "prettier" },
+        angular = { "prettier" },
+        css = { "prettier" },
+        flow = { "prettier" },
+        javascriptreact = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+        jsx = { "prettier" },
+        acss = { "prettier" },
+        typescript = { "prettier" },
+        typescriptreact = { "prettier" },
+        vue = { "prettier" },
+        tsx = { "prettier" },
+        yaml = { "prettier" },
       },
       format_on_save = { timeout_ms = 500, lsp_format = "fallback" },
       ["_"] = { "trim_whitespace" },
