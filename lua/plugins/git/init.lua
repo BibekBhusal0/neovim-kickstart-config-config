151536e24d03c4b85248c9b03ce3e5a950da7a46
Bibek Bhusal <bibekbhusal04@gmail.com>
2026-07-29 21:37:14 +0545

    feat: ✨ add nvim-ghrelease plugin for GitHub releases

--- lua/plugins/git/init.lua
+++ lua/plugins/git/init.lua
@@ -21,8 +21,9 @@ return {
 
   require "plugins.git.diffview",
   require "plugins.git.gitsigns",
-  require "plugins.git.pipeline",
   require "plugins.git.octo",
-  require "plugins.git.worktree",
+  require "plugins.git.pipeline",
+  require "plugins.git.release",
   require "plugins.git.utils",
+  require "plugins.git.worktree",
 }
---
+++ lua/plugins/git/release.lua
@@ -0,0 +1,5 @@
+return {
+  "mesirendon/nvim-ghrelease",
+  opts = { keymaps = false },
+  keys = { { "<leader>GR", "<cmd>GhRelease<cr>", desc = "GitHub Release: create" } },
+}
